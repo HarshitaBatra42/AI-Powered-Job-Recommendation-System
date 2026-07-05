40919a4f883094de1d7a41cee747a6dab5c40edb
Harshita batra <Harshitabatra607@gmail.com>
2026-07-05 08:33:05 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,11 +1,12 @@
+import os
 import psycopg2
 
 def get_connection():
     conn = psycopg2.connect(
-        host="localhost",
-        database="Career_dashboard",
-        user="postgres",
-        password="0000"
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT")
     )
-
     return conn
